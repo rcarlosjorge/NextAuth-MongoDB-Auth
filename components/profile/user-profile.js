a6314eea45f6db70b25a2de31c5e770512a03508
rcarlosjorge <carlosepjorge@gmail.com>
2024-04-27 17:59:22 -0400

    Project version 1 completed

---
+++ components/profile/user-profile.js
@@ -0,0 +1,50 @@
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
+
+
+
+function UserProfile() {
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
+
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
+
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch("/api/user/change-password", {
+      method: "PATCH",
+      body: JSON.stringify(passwordData),
+      headers: {
+        "content-type": "application/json",
+      },
+      credentials: 'include',
+    });
+  
+    const data = await response.json();
+  
+    if (!response.ok) {
+      console.log("Error:", data);
+    } else {
+      console.log("Password changed successfully:", data);
+    }
+  }
+  
+  return (
+    <section className={classes.profile}>
+      <h1>Your User Profile</h1>
+      <ProfileForm onChangePassword={changePasswordHandler} />
+    </section>
+  );
+}
+
+export default UserProfile;
